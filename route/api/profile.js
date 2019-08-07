22834dc08079c7eb55407ebd740c65b53c6ee791
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-07 20:41:25 +0700

    Add and Delete experience

--- route/api/profile.js
+++ route/api/profile.js
@@ -42,7 +42,6 @@ router.post('/',auth,[
     skills,
     bio,
     githubusername,
-    experience,
     youtube,
     twitter,
     facebook,
@@ -139,6 +138,81 @@ router.get('/user/:user_id',async (req,res) => {
 })
 
 
+// @route    Post api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+
+
+router.delete('/',auth,async(req,res) => {
+  try {
+    //@todo - remove users posts
+
+    //Remove profile
+    await Profile.findOneAndRemove({user:req.user.id});
+    //Remove user
+    await User.findOneAndRemove({_id:req.user.id});
+
+    res.json({msg:"User deleted"});
+    res.json()
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('Server Error');
+  }
+})
+
+
+// @route    Put api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+
+
+router.put('/experience',[auth,[
+  check('title','Title is required').not().isEmpty(),
+  check('company','Company is required').not().isEmpty(),
+  check('from','From is required').not().isEmpty(),
+
+]],async (req,res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    res.status(401).json({errors:errors.array()});
+  }
+
+  const {title, company,location,from,to,current,description} = req.body;
+  const newExp = {title, company,location,from,to,current,description};
+  try {
+    
+    const profile = await Profile.findOne({user:req.user.id});
+    if(!profile) {
+      return res.status(401).json({msg:"Profile not found"})
+    }
+    profile.experience.unshift(newExp);
+    await profile.save();
+     res.json(profile);
+  } catch (error) {
+    console.log(error);
+    res.send(500).json("Server Error");
+  }
+})
+
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     DELETE profile experience by exp ID
+// @access   Private
+
+router.delete('/experience/:exp_id',auth,async (req,res) => {
+  try {
+    const profile = await Profile.findOne({user:req.user.id});
+
+    const indexToDel = profile.experience.map(item =>item._id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(indexToDel,1);
+    await profile.save();
+    res.json(profile);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('Server Error');
+  }
+})
 
 
 module.exports = router
