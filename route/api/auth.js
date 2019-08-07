89b26090c05c9381e7b725653480a9a1cf0576a1
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-07 11:48:06 +0700

    login

--- route/api/auth.js
+++ route/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator')
 const User = require('../../modules/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 // @route    GET api/auth
 // @desc     Test
 // @access   public
@@ -13,6 +17,56 @@ router.get('/',auth, async (req,res)=> {
   catch(err) {
     console.log(err);
     res.status(500).json('Server Error')
+  }
+})
+
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   public
+
+
+router.post('/',[ 
+  check('email','Please include a valid email').isEmail(),
+  check('password','Password is required').exists()
+], async (req,res)=> { 
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()})
+  }
+  const {email,password} = req.body;
+
+  try {
+
+    // See if user exists
+    let user = await User.findOne({email});
+    if(!user) {
+      res.status(400).json({
+        errors:[{msg:'Invalid Credentials'}]
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch) {
+      res.status(400).json({
+        errors:[{msg:'Invalid Password'}]
+      })
+    }
+
+      const payload = {
+        user: {
+          id:user.id
+        } 
+      }
+      jwt.sign(payload,config.get('jwtSecret'),{expiresIn:'1h'},(err,token)=> {
+        if(err) throw err;
+        res.json({token})
+      })
+  }
+  catch(err) {
+    console.log(err.massage);
+    res.status(500).send('Server error')
   }
 })
 
