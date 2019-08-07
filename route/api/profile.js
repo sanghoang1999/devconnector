c98157d1a9c54cb3672635de14faed5609f97820
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-07 19:14:59 +0700

    Get all profile & Get profile by user ID

--- route/api/profile.js
+++ route/api/profile.js
@@ -102,5 +102,43 @@ router.post('/',auth,[
 })
 
 
+// @route    Get api/profile
+// @desc     Get all profile
+// @access   Public
+
+router.get('/',async (req,res) => {
+  try {
+    const profiles = await Profile.find().populate('user',['name','avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Server Error");
+  }
+})
+
+
+// @route    Get api/profile/user/:user_id
+// @desc     Get profile by user_id
+// @access   Public
+
+router.get('/user/:user_id',async (req,res) => {
+  try {
+    const profile = await Profile.find({user:req.params.user_id}).populate('user',['name','avatar']);
+    
+    if(!profile) {
+      return res.status(400).json({msg:'Profile not found'});
+    }
+    res.json(profile);
+  } catch (error) {
+    console.log(error);
+    if(error.kind=="ObjectId") {
+      return res.status(400).json({msg:'Profile not found'});
+    }
+    res.status(500).json("Server Error");
+  }
+})
+
+
+
 
 module.exports = router
