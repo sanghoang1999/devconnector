3cd428393b7623bd2abc8bea05edaa00b42a8aff
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-07 11:32:50 +0700

    add middleware auth jwt

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('config')
+
+module.exports = function(req, res, next) {
+  // Get the token from header
+  const token = req.header('x-auth-token');
+  // check if no token
+  if(!token) {
+    return res.status(401).json({msg:'no token,authorization denied'})
+  }
+  try{
+    const decoded = jwt.verify(token,config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).json({msg:'Token is not valid'})
+  }
+}
--- route/api/auth.js
+++ route/api/auth.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const User = require('../../modules/User');
 // @route    GET api/auth
 // @desc     Test
 // @access   public
-router.get('/',(req,res)=> {
-  res.send('auth route');
+router.get('/',auth, async (req,res)=> {
+  try{
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).json('Server Error')
+  }
 })
 
 
