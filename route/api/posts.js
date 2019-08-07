33e04d79a148eba2b121b850b09cd9bebb40cc67
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-07 22:19:01 +0700

    Create Post

--- route/api/posts.js
+++ route/api/posts.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../modules/Profile');
+const User = require('../../modules/User');
+const Post= require('../../modules/Post');
+const { check, validationResult } = require('express-validator')
+const config = require('config');
 
-// @route    GET api/posts
-// @desc     Test
+// @route    Post api/posts
+// @desc     Create a post
 // @access   public
-router.get('/',(req,res)=> {
-  res.send('posts route');
+router.post('/',[auth,[
+  check('text','Text is required').not().isEmpty(),
+]],async (req,res)=> {
+  const errors = validationResult(req);
+  if(!errors.isEmpty) {
+    return res.status(400).json({errors:errors.array()})
+  }
+  try {
+    const user = await User.findById({_id:req.user.id}).select('-password');
+    const newPost= new Post({
+      text:req.body.text,
+      name:user.name,
+      avatar:user.avatar,
+      user:req.user.id,
+    });
+    const post = await newPost.save();
+    res.json(post);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json('Server Error');
+  }
 })
 
 
