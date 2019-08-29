c46c9f17c85d3e05a2185cc33def8e50caeb5893
Sang Hoàng <sang.hoang.1999@hcmut.edu.vn>
2019-08-29 20:23:57 +0700

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const connectDB  = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,24 +10,26 @@ connectDB();
 
 //init Middleware
 app.use(cors());
-app.use(express.json({ extended:true}))
-
-app.get('/',(req,res)=> {
-  res.send('API Running');
-})
-
+app.use(express.json({ extended: true }));
 
 //Define route
 
-app.use('/api/users',require('./route/api/users'))
-app.use('/api/auth',require('./route/api/auth'))
-app.use('/api/profile',require('./route/api/profile'))
-app.use('/api/posts',require('./route/api/posts'))
+app.use("/api/users", require("./route/api/users"));
+app.use("/api/auth", require("./route/api/auth"));
+app.use("/api/profile", require("./route/api/profile"));
+app.use("/api/posts", require("./route/api/posts"));
 
-
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
-})
+});
